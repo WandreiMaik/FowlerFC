2b7ec71ce8e059b5de22b6a16aa855ef9cc7adae
Maik Wandrei <maik_546@yahoo.de>
2015-04-23 11:30:06 +0200

    Added changes.
    Changed something in Customer.statement for the lulz!

--- src/Customer.java
+++ src/Customer.java
@@ -15,35 +15,32 @@ class Customer {
         return name;
     };
     public String statement() {
-        double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration rentals = this.rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
             frequentRenterPoints += each.getFrequentRenterPoints();
-// add frequent renter points
-            frequentRenterPoints ++;
-// add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                    && each.getDaysRented() > 1) frequentRenterPoints ++;
 //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" +
-                    String.valueOf(each.getCharge())
-                    + "\n";
-            totalAmount += each.getCharge();
+                    String.valueOf(each.getCharge()) + "\n";
         }
 //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) +
-                "\n";
+        result += "Amount owed is " +
+                String.valueOf(getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints)
-                + " frequent renter points";
+                +
+                " frequent renter points";
         return result;
     }
-
-    private double amountFor(Rental aRental) {
-        return aRental.getCharge();
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration rentals = this.rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getCharge();
+        }
+        return result;
     }
-
 }
     
