405fd5aa51d11bb228de5cb8783b8dc7d9b686da
alisaIT <alisa-koksjuk@gmx.de>
2015-04-23 11:50:27 +0200

    Edit constructor of Movie

--- src/Movie.java
+++ src/Movie.java
@@ -6,7 +6,7 @@ public class Movie {
     private int priceCode;
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
-        priceCode = newpriceCode;
+        setPriceCode(priceCode);
     }
     double getCharge(int daysRented)
     {
