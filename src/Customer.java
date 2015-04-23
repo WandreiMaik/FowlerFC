1afc6a98df332a1a49d195e3eb508e9630c94ccf
Norman Naujokat <norman_naujokat@web.de>
2015-04-23 11:34:49 +0200

    Diverse Changes in Customer.statement and added new method
    Diverse Changes in Customer.statement and added new method in Customer

--- src/Customer.java
+++ src/Customer.java
@@ -14,13 +14,12 @@ class Customer {
     public String getName (){
         return name;
     };
+
     public String statement() {
-        int frequentRenterPoints = 0;
         Enumeration rentals = this.rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            frequentRenterPoints += each.getFrequentRenterPoints();
 //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" +
                     String.valueOf(each.getCharge()) + "\n";
@@ -28,11 +27,22 @@ class Customer {
 //add footer lines
         result += "Amount owed is " +
                 String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints)
-                +
+        result += "You earned " +
+                String.valueOf(getTotalFrequentRenterPoints()) +
                 " frequent renter points";
         return result;
     }
+    private int getTotalFrequentRenterPoints(){
+        int result = 0;
+        Enumeration rentals = this.rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
+        return result;
+    }
+
+
     private double getTotalCharge() {
         double result = 0;
         Enumeration rentals = this.rentals.elements();
